79a68727e2223f2cd2c20f4db1192fb6ea2a2550
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-08 15:48:48 -0500

    Updates

--- assets/file-spreadsheet-D38maR5J.js
+++ assets/file-spreadsheet-D38maR5J.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BXv2QlSA.js";/**
+import{c as a}from"./index-D5imZCe3.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/text-B8Y0g5WX.js
+++ assets/text-B8Y0g5WX.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BXv2QlSA.js";/**
+import{c as e}from"./index-D5imZCe3.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

42578875f2cf0f3f26b10434c9f1734cc3f3f8c3
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-08 14:55:37 -0500

    Updates

--- assets/file-spreadsheet-DZgg-ZH8.js
+++ assets/file-spreadsheet-DZgg-ZH8.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CdExO2oy.js";/**
+import{c as a}from"./index-DeH1ng3M.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/text-DF7tHSYh.js
+++ assets/text-DF7tHSYh.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CdExO2oy.js";/**
+import{c as e}from"./index-DeH1ng3M.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

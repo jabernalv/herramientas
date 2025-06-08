930d6ab23281d850026e65116d6f927722f0fc61
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-08 14:54:16 -0500

    Updates

--- assets/file-spreadsheet-EuxlcbIH.js
+++ assets/file-spreadsheet-EuxlcbIH.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DmzuQppI.js";/**
+import{c as a}from"./index-CdExO2oy.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/text-DMYLrvfu.js
+++ assets/text-DMYLrvfu.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DmzuQppI.js";/**
+import{c as e}from"./index-CdExO2oy.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

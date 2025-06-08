0565c02ff2c7a3b0c689b0b63cf9f0d51a4eb6d5
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-08 16:25:25 -0500

    Updates

--- assets/file-spreadsheet-BU1nb7Tm.js
+++ assets/file-spreadsheet-BU1nb7Tm.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-kcM8xMh0.js";/**
+import{c as a}from"./index-B708KNLo.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

8748f8f529f961ed667af6420f4e5d26fe698c07
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-09 13:09:26 -0500

    Updates

--- assets/text-BgMOF0dP.js
+++ assets/text-BgMOF0dP.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-ZmRgUgie.js";/**
+import{c as e}from"./index-VGqBE0HY.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

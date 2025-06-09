d5597dd651a253eb9929ef95bc82fb6dddd1e74e
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-09 14:30:56 -0500

    Updates

--- assets/file-spreadsheet-q0pAn587.js
+++ assets/file-spreadsheet-q0pAn587.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-pvpTCwg_.js";/**
+import{c as a}from"./index-Bg_nJfz-.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

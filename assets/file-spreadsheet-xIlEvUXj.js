a1250d3987a157d76a06cd4ed265c47c84458456
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-10 10:22:29 -0500

    Updates

--- assets/file-spreadsheet-xIlEvUXj.js
+++ assets/file-spreadsheet-xIlEvUXj.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Ac5xxfl7.js";/**
+import{c as a}from"./index-Czk4Hi60.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/hash-DBloIo16.js
+++ assets/hash-DBloIo16.js
@@ -1,4 +1,4 @@
-import{c as y}from"./index-Ac5xxfl7.js";/**
+import{c as y}from"./index-Czk4Hi60.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

c31103261df63474c9b3f9694ce1cc417ba43933
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-09 23:03:57 -0500

    Updates

--- assets/file-spreadsheet-B3RVDDcz.js
+++ assets/file-spreadsheet-B3RVDDcz.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CdBGwCQ1.js";/**
+import{c as a}from"./index-CXmY0rBl.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/text-BPswmxcT.js
+++ assets/text-BPswmxcT.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CdBGwCQ1.js";/**
+import{c as e}from"./index-CXmY0rBl.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

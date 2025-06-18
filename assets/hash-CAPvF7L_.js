8981b17f3a8101b5348d64bd62f816f5a5fa4678
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-06-17 19:52:31 -0500

    Updates

--- assets/hash-CAPvF7L_.js
+++ assets/hash-CAPvF7L_.js
@@ -1,4 +1,4 @@
-import{c as y}from"./index-BuN9iG7y.js";/**
+import{c as y}from"./index-Daf8kAT6.js";/**
  * @license lucide-vue-next v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.

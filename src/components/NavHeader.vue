e43f0fd4cf22585bfeb93bb3cd96737e4379f767
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-07-01 18:49:41 -0500

    ✅ Build y deploy exitosos: eyedropper bloqueado si el color está bloqueado, UX final estable.

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -186,6 +186,17 @@ const items: PrimeMenuItem[] = [
   max-width: 1280px;
 }
 .nav-gradient {
+  /* fallback for old browsers */
+  background: #6a11cb;
+
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(
+    to right,
+    rgba(72, 107, 173, 0.9),
+    rgba(7, 9, 255, 1)
+  );
+
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   background: linear-gradient(
     75deg,
     rgba(72, 107, 173, 1) 0%,

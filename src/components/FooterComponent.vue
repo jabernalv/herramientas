3d24ba78a37ae6f40398c728af14ff7784cb6b73
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-07-11 20:01:47 -0500

    feat: Agregada la función de contar visitas

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,8 +5,27 @@
     <p class="text-sm">
       Liberado bajo licencia MIT | Jairo Bernal, {{ new Date().getFullYear() }}
     </p>
-    <span id="visitas" class="text-gray-400 text-xs">Visitas: ...</span>
+    <span id="visitas" class="text-gray-400 text-xs"
+      >Visitas: {{ visitas }}</span
+    >
   </footer>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+let visitas = ref(0);
+
+onMounted(() => {
+  fetch(
+    "https://intranet.dirigiendoproyectos.com/contador-visitas.php?type=herramientas"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      visitas.value = data.visitas;
+    })
+    .catch((err: any) => {
+      visitas.value = 0;
+    });
+});
+</script>
 <style scoped></style>

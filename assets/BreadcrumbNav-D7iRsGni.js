ab30ed80e19a68fb7157705c76c4a355432be76f
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-07-21 18:20:18 -0500

    Updates

---
+++ assets/BreadcrumbNav-D7iRsGni.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./BreadcrumbNav.vue_vue_type_script_setup_true_lang-19izqj3U.js";import"./vue-vendor-D8N656sj.js";import"./primevue-DrPXlnFP.js";import"./index-DCzI8z5Q.js";export{o as default};
--- assets/FooterComponent-Bfo_DLzI.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./index-D-CNp7HO.js";import"./vue-vendor-D8N656sj.js";import"./primevue-qOHAer5i.js";export{o as default};

06dd6615924eb9fcd57e41b464d8a46d91f9d3b1
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-07-21 18:51:34 -0500

    Updates

--- assets/FooterComponent-DOQY3sF0.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./index-Cz-1_Owd.js";import"./vue-vendor-D8N656sj.js";import"./primevue-DrPXlnFP.js";export{o as default};
---
+++ assets/FooterComponent-KTt8Wil4.js
@@ -0,0 +1,1 @@
+import{g as o}from"./index-C99OtVWq.js";import"./vue-vendor-D8N656sj.js";import"./primevue-DrPXlnFP.js";export{o as default};
